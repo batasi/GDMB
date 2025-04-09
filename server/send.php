6afcec038b9759dc9909f361ef03a4b509cd2dca
batasi <batasisolomon029@gmail.com>
2025-04-09 10:43:19 +0300

    mailer

--- server/send.php
+++ server/send.php
@@ -16,9 +16,7 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
         $mail->SMTPAuth = true;
         $mail->Username = $config['username'];
         $mail->Password = $config['password'];
-        $mail->SMTPSecure = $config['secure'] === 'ssl'
-        ? PHPMailer::ENCRYPTION_SMTPS
-        : PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->SMTPSecure = $config['secure'];
         $mail->Port = $config['port'];
 
         //Recipients
